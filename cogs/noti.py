c280d8afc711ddd2e93559e8e596e528c5c4eac0
rainy10 <64578776+rainy10@users.noreply.github.com>
2021-07-23 18:50:35 +0900

    Update noti.py

--- cogs/noti.py
+++ cogs/noti.py
@@ -1,6 +1,8 @@
 import discord
 from discord import message
 from discord.ext import commands
+import json
+import os.path
 
 intents = discord.Intents.default()
 intents.members = True
@@ -13,6 +15,12 @@ class noti(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, msg):
         pass
+
+    @commands.command()
+    async def set_noti_channel(self, ctx, channel):
+        print(f'servers/{channel}')
+        if os.path.isfile(f'servers/{channel}'):
+            pass
 
 def setup(bot: commands.Bot):
     bot.add_cog(noti(bot))
